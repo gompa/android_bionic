ee0c554fac7a79943d09c1b6bde610bd5eae817a
Gompa <gompa@h-bomb.nl>
2016-07-27 19:37:16 +0200

    fix build error

--- libc/include/uchar.h
+++ libc/include/uchar.h
@@ -33,10 +33,12 @@
 #include <wchar.h>
 
 __BEGIN_DECLS
-#if __STDC_VERSION__ >= 201112L && !defined(__cplusplus)
-#define __STD_UTF_16__ 1
-#define __STD_UTF_32__ 1
+
+#if !defined(__cplusplus)
+typedef __CHAR16_TYPE__ char16_t;
+typedef __CHAR32_TYPE__ char32_t;
 #endif
+
 
 size_t c16rtomb(char* __restrict, char16_t, mbstate_t* __restrict);
 size_t c32rtomb(char* __restrict, char32_t, mbstate_t* __restrict);
